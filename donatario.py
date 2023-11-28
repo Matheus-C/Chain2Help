a08ce0c3a7e30461b1397a983a156edf41a90396
Matheus-C <matheuscustodio@id.uff.br>
2023-11-28 11:41:36 -0300

    melhorias no front com bootstrap

---
+++ donatario.py
@@ -0,0 +1,10 @@
+
+
+class Donatario:
+    def __init__(self, id, name, page, total):
+        self.id = id
+        self.name = name
+        self.page = page
+        self.total = total
+        
+    
