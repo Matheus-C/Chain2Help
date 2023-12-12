b40f25a536f7b04499d559a1a7ad9f68ce011c72
Gabriel Figueiredo <gabrielfigueiredov@gmail.com>
2023-12-12 14:54:32 -0300

    Automated flow execution

--- app/wallet.py
+++ app/wallet.py
@@ -5,11 +5,9 @@ import json
 from app.balance import Balance
 from app.log import logger
 from app.outputs import Error, Notice, Voucher
-from eth_abi import decode, encode
+from eth_abi import encode
 from app.eth_abi_ext import decode_packed
 
-# Function selector to be called during the execution of a voucher that transfers funds,
-# which corresponds to the first 4 bytes of the Keccak256-encoded result of "transfer(address,uint256)"
 TRANSFER_FUNCTION_SELECTOR = b'\xa9\x05\x9c\xbb'
 
 _accounts = dict[str: Balance]()
@@ -25,30 +23,10 @@ def _balance_get(account) -> Balance:
 
 
 def balance_get(account) -> Balance:
-    """Retrieve the balance of all ERC-20 and ERC-721 tokens for `account`"""
-
     logger.info(f"Balance for '{account}' retrieved")
     return _balance_get(account)
 
-def balance_list() -> list[Balance]:
-    """Retrieve the balance of all ERC-20 and ERC-721 tokens for `account`"""
-
-    logger.info("Balance list")
-    return _accounts
-
-
 def erc20_deposit_process(payload:str):
-    '''
-    Process the ABI-encoded input data sent by the ERC20Portal
-    after an ERC-20 deposit
-        Parameters:
-            payload (str): the binary input data as hex string.
-
-        Returns:
-            notice (Notice): A notice whose payload is the hex value for an ERC-20 deposit JSON.
-            report (Error): A report detailing the operation's failure reason.
-    '''
-    # remove the '0x' prefix and convert to bytes
     binary_payload = bytes.fromhex(payload[2:])
     try:
         account, erc20, amount = _erc20_deposit_parse(binary_payload)
@@ -61,53 +39,23 @@ def erc20_deposit_process(payload:str):
 
 
 def _erc20_deposit_parse(binary_payload: bytes):
-    '''
-    Retrieve the ABI-encoded input data sent by the ERC20Portal
-    after an ERC-20 deposit.
-
-        Parameters:
-            binary_payload (bytes): ABI-encoded input
-
-        Returns:
-            A tuple containing:
-                account (str): address which owns the tokens
-                erc20 (str): ERC-20 contract address
-                amount (int): amount of deposited ERC-20 tokens
-    '''
     try:
-        input_data = decode_packed(
-            ['bool',     # Is a valid deposit
-             'address',  # Address of the ERC-20 contract
-             'address',  # Address which deposited the tokens
-             'uint256'], # Amount of ERC-20 tokens being deposited
-            binary_payload
-        )
+        input_data = decode_packed(['bool', 'address', 'address', 'uint256'], binary_payload)
 
         valid = input_data[0]
         if not valid:
             raise ValueError("Invalid deposit with 'False' success flag")
+
         erc20 = input_data[1]
         account = input_data[2]
         amount = input_data[3]
+
         return account, erc20, amount
     except Exception as error:
-        raise ValueError(
-            "Payload does not conform to ERC-20 transfer ABI") from error
+        raise ValueError("Payload does not conform to ERC-20 transfer ABI") from error
 
 
 def _erc20_deposit(account, erc20, amount):
-    '''
-    Deposit ERC-20 tokens in account.
-
-        Parameters:
-            account (str): address who owns the tokens.
-            erc20 (str): address of the ERC-20 contract.
-            amount (float): amount of tokens to deposit.
-
-        Returns:
-            notice (Notice): A notice whose payload is the hex value for an
-            ERC-20 deposit JSON.
-    '''
     balance = _balance_get(account)
     balance._erc20_increase(erc20, amount)
 
@@ -122,18 +70,6 @@ def _erc20_deposit(account, erc20, amount):
     return Notice(json.dumps(notice_payload))
 
 def erc20_withdraw(account, erc20, amount):
-    '''
-    Extract ERC-20 tokens from account.
-
-        Parameters:
-            account (str): address who owns the tokens.
-            erc20 (str): address of the ERC-20 contract.
-            amount (float): amount of tokens to withdraw.
-
-        Returns:
-            voucher (Voucher): A voucher that transfers `amount` tokens to
-            `account` address.
-    '''
     balance = _balance_get(account)
     balance._erc20_decrease(erc20, amount)
 
@@ -145,18 +81,6 @@ def erc20_withdraw(account, erc20, amount):
 
 
 def erc20_transfer(account, to, erc20, amount):
-    '''
-    Transfer ERC-20 tokens from `account` to `to`.
-
-        Parameters:
-            account (str): address who owns the tokens.
-            to (str): address to send tokens to.
-            erc20 (str): address of the ERC-20 contract.
-            amount (int): amount of tokens to transfer.
-
-        Returns:
-            notice (Notice): A notice detailing the transfer operation.
-    '''
     try:
         balance = _balance_get(account)
         balance_to = _balance_get(to)
@@ -173,10 +97,10 @@ def erc20_transfer(account, to, erc20, amount):
                 "amount": amount
             }
         }
-        logger.info(f"'{amount} {erc20}' tokens transferred from "
-                    f"'{account}' to '{to}'")
+        logger.info(f"'{amount} {erc20}' tokens transferred from '{account}' to '{to}'")
         return Notice(json.dumps(notice_payload))
     except Exception as error:
         error_msg = f"{error}"
         logger.debug(error_msg, exc_info=True)
+
         return Error(error_msg)
--- setup_campaign.sh
+++ setup_campaign.sh
@@ -1,12 +1,15 @@
-timestamp=$(( $(date +%s) + 30 ))
-end_date=$(($timestamp + 300))
+cd ../rollups-examples/common-contracts/
+yarn deploy
 
-cd ../rollups-examples/frontend-console/
+cd ../frontend-console/
 
 WALLET_0=0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
 WALLET_1=0x70997970C51812dc3A010C7d01b50e0d17dc79C8
 
 ERC20=0x2797a6a6D9D94633BA700b52Ad99337DdaFA3f52
+
+timestamp=$(( $(date +%s) + 60 ))
+end_date=$(($timestamp + 900))
 
 echo "===========> Create an campaign starting in $timestamp and ending in $end_date"
 yarn start input send --payload '{
@@ -21,14 +24,44 @@ yarn start input send --payload '{
     }
 }'
 
+echo ""
+echo ""
+echo "===========> Check the balance of the account $WALLET_0 (creator)"
+yarn start inspect --url http://localhost:8080/inspect/ --payload "balance/$WALLET_0"
+
+echo ""
+echo ""
+echo "===========> List all campaigns"
 yarn start inspect --url http://localhost:8080/inspect/ --payload "campaign"
 
+echo ""
+echo ""
+echo "===========> Check the campaign created"
 yarn start inspect --url http://localhost:8080/inspect/ --payload "campaign/0"
 
-cast send $ERC20 "transfer(address,uint256)(bool)" $WALLET_1 2000 --mnemonic "test test test test test test test test test test test junk" --mnemonic-index 0 --rpc-url "http://localhost:8545"
+echo ""
+echo ""
+echo "===========> Transfer founds of account $WALLET_0 to $WALLET_1"
+cast send '0x2797a6a6D9D94633BA700b52Ad99337DdaFA3f52' "transfer(address,uint256)(bool)" '0x70997970C51812dc3A010C7d01b50e0d17dc79C8' 2000 --mnemonic "test test test test test test test test test test test junk" --mnemonic-index 0 --rpc-url "http://localhost:8545"
 
+echo ""
+echo ""
+echo "===========> Check the founds of account $WALLET_1 before the deposit"
+yarn start inspect --url http://localhost:8080/inspect/ --payload "balance/$WALLET_1"
+
+echo ""
+echo ""
+echo "===========> Deposit 2000 founds of account $WALLET_1"
 yarn start erc20 deposit --amount 2000 --accountIndex 1
 
+echo ""
+echo ""
+echo "===========> Check the founds of account $WALLET_1 after the deposit"
+yarn start inspect --url http://localhost:8080/inspect/ --payload "balance/$WALLET_1"
+
+echo ""
+echo ""
+echo "===========> Donates 1000 founds of account $WALLET_1 to the created campaign"
 yarn start input send --accountIndex 1 --payload '{
     "method": "donate",
     "args": {
@@ -37,6 +70,81 @@ yarn start input send --accountIndex 1 --payload '{
     }
 }'
 
+echo ""
+echo ""
+echo "===========> Check the founds of account $WALLET_1 (donor) after the donation"
+yarn start inspect --url http://localhost:8080/inspect/ --payload "balance/$WALLET_1"
+
+echo ""
+echo ""
+echo "===========> Check the founds of account $WALLET_0 (receiver) after the donation"
+yarn start inspect --url http://localhost:8080/inspect/ --payload "balance/$WALLET_1"
+
+echo ""
+echo ""
+echo "===========> List all campaign 0 donations"
 yarn start inspect --url http://localhost:8080/inspect/ --payload "campaign/0/donations"
 
+echo ""
+echo ""
+echo "===========> Check the campaign founds updated"
 yarn start inspect --url http://localhost:8080/inspect/ --payload "campaign/0"
+
+echo ""
+echo ""
+echo "===========> End the campaign 0 before the ending date"
+yarn start input send --payload '{
+    "method": "end",
+    "args": {
+        "campaign_id": 0,
+        "force": true
+    }
+}'
+
+echo ""
+echo ""
+echo "===========> Campaign creator withdraws the money"
+yarn start input send --payload '{
+    "method": "withdraw",
+    "args": {
+        "erc20": "'$ERC20'",
+        "amount": 1000
+    }
+}'
+
+echo ""
+echo ""
+echo "===========> Check the founds of account $WALLET_0 (creator) after the withdraw"
+yarn start inspect --url http://localhost:8080/inspect/ --payload "balance/$WALLET_0"
+
+echo ""
+echo ""
+echo "===========> Account $WALLET_0 tries to withdraw more money than it has"
+yarn start input send --payload '{
+    "method": "withdraw",
+    "args": {
+        "erc20": "'$ERC20'",
+        "amount": 500
+    }
+}'
+
+echo ""
+echo ""
+echo "===========> List all notices generated from this interations"
+yarn start notice list --url http://localhost:8080/graphql
+
+echo ""
+echo ""
+echo "===========> Force time advance to check vouchers and reports generated"
+curl -H "Content-type: application/json" --data '{"id":1336,"jsonrpc":"2.0","method":"evm_increaseTime","params":[864010]}' http://localhost:8545
+
+echo ""
+echo ""
+echo "===========> List all vouchers generated from this interations"
+yarn start voucher list --url http://localhost:8080/graphql
+
+echo ""
+echo ""
+echo "===========> List all reports generated from this interations"
+yarn start report list --url http://localhost:8080/graphql
+
